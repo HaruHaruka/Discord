932b39a867c42a00b785a87192a224f9f4b53325
HaruHaruka <Haruka.Crescendia@gmail.com>
2017-06-18 16:54:15 +0800

    Random commands Youtube module under test

--- kumirei.py
+++ kumirei.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import time
 from discord.ext.commands import Bot
 
 
@@ -18,5 +20,28 @@ async def hello(*args):
 @kumirei.command()
 async def song(*args):
     pass
+
+
+@kumirei.command()
+async def mood(*args):
+    moods = ["悲しいです。。。", "今は幸せって感じ＾＾～", "泣きたい。。", "恋してるぅぅぅぅぅ", "怒ってます！"]
+    return await kumirei.say(moods[random.randint(0,len(moods) - 1)])
+
+
+@kumirei.command()
+async def dice(*args):
+    await kumirei.say("行くよ～")
+    faces = [1, 2, 3, 4, 5, 6]
+    value = faces[random.randint(0, len(faces) - 1)]
+    for item in faces:
+        if item == value:
+            return await kumirei.say("{}　でした！")
+
+
+@kumirei.command()
+async def omikuji(*args):
+    await kumirei.say("はい！おみくじを引くよ～\n今日の運は。。。")
+    luck = ["大凶", "凶", "小吉", "大吉", "小凶", "吉"]
+    return await kumirei.say(luck[(random.randint(0, len(luck) - 1))] + "でした！")
 
 kumirei.run("MzIzNTYzMzI0MDUyODY1MDM0.DCAU2w.Tp3IB3Uv0chcv8vMqHgPkvmxOAY")
